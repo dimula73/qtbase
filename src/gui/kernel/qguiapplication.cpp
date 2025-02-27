91c2e74b51785cc77c8d4c6180d1823b0e248f57
Dmitry Kazakov <dimula73@gmail.com>
2025-02-27 11:12:18 +0100

    Add workaround for handling table press correctly in WinInk mode
    Original problem: widgets do not get synthesized mouse-down and
    mouse-press events until the stylus is released
    
    Reason: if the app accepts the event, WndProc should report
    that to the system (by returning true). This is the only way to
    prevent Windows from starting some system-wide gestures, like
    click+hold -> right button click. If we ignore the event, then
    OS postpones all synthesized mouse events until the entire gesture
    is completed.
    
    The patch implements a "hackish" workaround for the original problem
    by using the following rules:
    
    1) All tablet-move events are ignored (without synthesized mouse events
       OS doesn't generate any Enter/Leave events)
    
    2) All not-accepted tablet press- and release-events and also reported as
       ignored (without it D&D doesn't work).
    
    3) All accepted tablet press- and release-events are reported as "accepted",
       **but** we artificially synthesize mouse events for them.
    
    TODO: there are still one problem:
    
    1) Perhaps this synthesizeMouseEvent() is not needed at all. But we should
       first check if Qt relies on these synthesized messages anywhere in the
       code or not.
    
    See bug: https://bugs.kde.org/show_bug.cgi?id=406668

--- src/gui/kernel/qguiapplication.cpp
+++ src/gui/kernel/qguiapplication.cpp
@@ -2998,6 +2998,7 @@ void QGuiApplicationPrivate::processTabletEvent(QWindowSystemInterfacePrivate::T
 
     QGuiApplication::sendSpontaneousEvent(window, &tabletEvent);
     pointData.state = e->buttons;
+    e->eventAccepted = tabletEvent.isAccepted();
     if (!tabletEvent.isAccepted()
         && !QWindowSystemInterfacePrivate::TabletEvent::platformSynthesizesMouse
         && qApp->testAttribute(Qt::AA_SynthesizeMouseForUnhandledTabletEvents)) {
