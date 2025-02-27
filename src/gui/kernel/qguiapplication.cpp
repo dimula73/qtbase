aef996fb1dd4599243629c2923410b6c4f491a54
Dmitry Kazakov <dimula73@gmail.com>
2025-02-27 11:12:18 +0100

    Add workaround for handling table press correctly in WinInk mode
    Original problem: widgets do not get synthesized mouse-down and
    mouse-press events until the stylus is released
    
    Reason: if the app accepts the event, WndProc should report
    that to the system (by returning true). This is the only way to
    prevent Windows from starting some system-wide gestures, like
    click+hold -> right button click. If we ignore the event, then
    OS postpones all synthesized mouse events until the entire gesture
    is completed.
    
    The patch implements a "hackish" workaround for the original problem
    by using the following rules:
    
    1) All tablet-move events are ignored (without synthesized mouse events
       OS doesn't generate any Enter/Leave events)
    
    2) All not-accepted tablet press- and release-events and also reported as
       ignored (without it D&D doesn't work).
    
    3) All accepted tablet press- and release-events are reported as "accepted",
       **but** we artificially synthesize mouse events for them.
    
    TODO: there are still one problem:
    
    1) Perhaps this synthesizeMouseEvent() is not needed at all. But we should
       first check if Qt relies on these synthesized messages anywhere in the
       code or not.
    
    See bug: https://bugs.kde.org/show_bug.cgi?id=406668

--- src/gui/kernel/qguiapplication.cpp
+++ src/gui/kernel/qguiapplication.cpp
@@ -2982,6 +2982,7 @@ void QGuiApplicationPrivate::processTabletEvent(QWindowSystemInterfacePrivate::T
 
     QGuiApplication::sendSpontaneousEvent(window, &tabletEvent);
     pointData.state = e->buttons;
+    e->eventAccepted = tabletEvent.isAccepted();
     if (!tabletEvent.isAccepted()
         && !QWindowSystemInterfacePrivate::TabletEvent::platformSynthesizesMouse
         && qApp->testAttribute(Qt::AA_SynthesizeMouseForUnhandledTabletEvents)) {
--- src/gui/kernel/qwindowsysteminterface.cpp
+++ src/gui/kernel/qwindowsysteminterface.cpp
@@ -894,7 +894,7 @@ bool QWindowSystemInterface::handleTabletEvent(QWindow *window, ulong timestamp,
                                                qreal tangentialPressure, qreal rotation, int z,
                                                Qt::KeyboardModifiers modifiers)
 {
-    return handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletEvent>(window,
+    return handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletEvent, SynchronousDelivery>(window,
         timestamp,
         QHighDpi::fromNativeLocalPosition(local, window),
         QHighDpi::fromNativeGlobalPosition(global, window),
@@ -952,8 +952,8 @@ bool QWindowSystemInterface::handleTabletEnterLeaveProximityEvent(QWindow *windo
     Q_UNUSED(z);
     Q_UNUSED(modifiers);
     return inProximity
-        ? handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletEnterProximityEvent>(timestamp, device)
-        : handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletLeaveProximityEvent>(timestamp, device);
+        ? handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletEnterProximityEvent, SynchronousDelivery>(timestamp, device)
+        : handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletLeaveProximityEvent, SynchronousDelivery>(timestamp, device);
 }
 
 bool QWindowSystemInterface::handleTabletEnterLeaveProximityEvent(QWindow *window, const QPointingDevice *device,
@@ -974,7 +974,7 @@ bool QWindowSystemInterface::handleTabletEnterProximityEvent(ulong timestamp, in
     const QPointingDevice *device = QPointingDevicePrivate::tabletDevice(QInputDevice::DeviceType(deviceType),
                                                                          QPointingDevice::PointerType(pointerType),
                                                                          QPointingDeviceUniqueId::fromNumericId(uid));
-    return handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletEnterProximityEvent>(timestamp, device);
+    return handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletEnterProximityEvent, SynchronousDelivery>(timestamp, device);
 }
 
 void QWindowSystemInterface::handleTabletEnterProximityEvent(int deviceType, int pointerType, qint64 uid)
@@ -988,7 +988,7 @@ bool QWindowSystemInterface::handleTabletLeaveProximityEvent(ulong timestamp, in
     const QPointingDevice *device = QPointingDevicePrivate::tabletDevice(QInputDevice::DeviceType(deviceType),
                                                                          QPointingDevice::PointerType(pointerType),
                                                                          QPointingDeviceUniqueId::fromNumericId(uid));
-    return handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletLeaveProximityEvent>(timestamp, device);
+    return handleWindowSystemEvent<QWindowSystemInterfacePrivate::TabletLeaveProximityEvent, SynchronousDelivery>(timestamp, device);
 }
 
 void QWindowSystemInterface::handleTabletLeaveProximityEvent(int deviceType, int pointerType, qint64 uid)
