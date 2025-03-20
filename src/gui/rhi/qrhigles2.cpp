27d343b1acfc01d7f3f3df2a4befcd31870c743d
Dmitry Kazakov <dimula73@gmail.com>
2025-03-20 18:24:39 +0100

    [win] Implement support for ANGLE's openGL backend
    When using openGL backend ANGLE requires the Y axis to be
    flipped, so we need to handle that in the QRhi backend.

--- src/gui/rhi/qrhigles2.cpp
+++ src/gui/rhi/qrhigles2.cpp
@@ -1222,6 +1222,8 @@ bool QRhiGles2::create(QRhi::Flags flags)
         flags.setFlag(QRhi::EnableFrameCompletionStatus, false);
     }
 
+    caps.yUpInNDC = ctx->handle()->isYUpInNDC();
+
     caps.unpackRowLength = !caps.gles || caps.ctxMajor >= 3;
 
     if (caps.gles)
@@ -1347,7 +1349,7 @@ bool QRhiGles2::isYUpInFramebuffer() const
 
 bool QRhiGles2::isYUpInNDC() const
 {
-    return true;
+    return caps.yUpInNDC;
 }
 
 bool QRhiGles2::isClipDepthZeroToOne() const
--- src/gui/rhi/qrhigles2_p.h
+++ src/gui/rhi/qrhigles2_p.h
@@ -1043,7 +1043,8 @@ public:
               unpackRowLength(false),
               perRenderTargetBlending(false),
               fenceSync(false),
-              needsFenceSyncWorkaround(false)
+              needsFenceSyncWorkaround(false),
+              yUpInNDC(true)
         { }
         int ctxMajor;
         int ctxMinor;
@@ -1109,6 +1110,7 @@ public:
         uint sampleVariables : 1;
         uint fenceSync : 1;
         uint needsFenceSyncWorkaround : 1;
+        uint yUpInNDC : 1;
     } caps;
     QGles2SwapChain *currentSwapChain = nullptr;
     QSet<GLint> supportedCompressedFormats;
