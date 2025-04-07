2c8a354250f2459eeb3078b01f255d013c70938f
Dmitry Kazakov <dimula73@gmail.com>
2025-04-07 11:25:44 +0200

    Fix a crash when opening multiple windows in Compatibility openGL profile
    legacyFuncs is different for every openGL context, so we shouldn't
    cache it in any way. Just request it every time we need it.
    QOpenGLVersionFunctionsFactory does already have a cache for it
    internally.

--- src/opengl/qopenglpaintengine_p.h
+++ src/opengl/qopenglpaintengine_p.h
@@ -151,9 +151,6 @@ public:
     };
 
     QOpenGL2PaintEngineExPrivate(QOpenGL2PaintEngineEx *q_ptr) :
-#if !QT_CONFIG(opengles2)
-            legacyFuncs(nullptr),
-#endif
             q(q_ptr),
             shaderManager(nullptr),
             width(0), height(0),
@@ -243,9 +240,6 @@ public:
     static void cleanupVectorPath(QPaintEngineEx *engine, void *data);
 
     QOpenGLExtensions funcs;
-#if !QT_CONFIG(opengles2)
-    QOpenGLFunctions_2_1 *legacyFuncs;
-#endif
 
     QOpenGL2PaintEngineEx* q;
     QOpenGLEngineShaderManager* shaderManager;
