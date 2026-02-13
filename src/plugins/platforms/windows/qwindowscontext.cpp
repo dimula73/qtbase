eb79fe7924704bbb880a7ca29a527f521c0e08e0
Dmitry Kazakov <dimula73@gmail.com>
2026-02-13 15:19:54 +0100

    Fix duplicated syntetic mouse events on WinTab
    WinTab synthesizes mouse events itself. That is, when it gets
    activated, the internal synthesizing should be disabled.

--- src/plugins/platforms/windows/qwindowscontext.cpp
+++ src/plugins/platforms/windows/qwindowscontext.cpp
@@ -252,6 +252,8 @@ bool QWindowsContext::initTablet()
 {
 #if QT_CONFIG(tabletevent)
     d->m_tabletSupport.reset(QWindowsTabletSupport::create());
+    // WinTab synthesizes mouse events itself
+    QWindowSystemInterfacePrivate::TabletEvent::setPlatformSynthesizesMouse(true);
     return true;
 #else
     return false;
@@ -262,6 +264,8 @@ bool QWindowsContext::disposeTablet()
 {
 #if QT_CONFIG(tabletevent)
     d->m_tabletSupport.reset();
+    // the alternative, WinInk doesn't synthesize mouse events
+    QWindowSystemInterfacePrivate::TabletEvent::setPlatformSynthesizesMouse(false);
     return true;
 #else
     return false;
