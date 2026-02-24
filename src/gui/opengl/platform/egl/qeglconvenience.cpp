e9d35e1afe1fe06a6cb1a5135f381561bd1a263f
Dmitry Kazakov <dimula73@gmail.com>
2026-02-24 14:46:24 +0100

    Fix EGL format being reset to 0 alpha instead of 2
    There is a weird design bug:
    
    1) The app requests a format of 10,10,10,2
    2) QWidgetPrivate::create() resets alpha to 8 as the window is considered "translucent"
    3) q_reduceConfigAttributes removes the unfitting alpha argument alltogether
    
    Now q_reduceConfigAttributes just tries to reduce
    alpha to 2 before removing it completely.

--- src/gui/opengl/platform/egl/qeglconvenience.cpp
+++ src/gui/opengl/platform/egl/qeglconvenience.cpp
@@ -201,15 +201,19 @@ bool q_reduceConfigAttributes(QList<EGLint> *configAttributes)
 
     i = configAttributes->indexOf(EGL_ALPHA_SIZE);
     if (i >= 0) {
-        configAttributes->remove(i,2);
-#if defined(EGL_BIND_TO_TEXTURE_RGBA) && defined(EGL_BIND_TO_TEXTURE_RGB)
-        i = configAttributes->indexOf(EGL_BIND_TO_TEXTURE_RGBA);
-        if (i >= 0) {
-            configAttributes->replace(i,EGL_BIND_TO_TEXTURE_RGB);
-            configAttributes->replace(i+1,true);
+        if (configAttributes->at(i + 1) > 2) {
+            (*configAttributes)[i + 1] = 2;
+        } else {
+            configAttributes->remove(i,2);
+    #if defined(EGL_BIND_TO_TEXTURE_RGBA) && defined(EGL_BIND_TO_TEXTURE_RGB)
+            i = configAttributes->indexOf(EGL_BIND_TO_TEXTURE_RGBA);
+            if (i >= 0) {
+                configAttributes->replace(i,EGL_BIND_TO_TEXTURE_RGB);
+                configAttributes->replace(i+1,true);
 
+            }
+    #endif
         }
-#endif
         return true;
     }
 
